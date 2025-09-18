e5dd8b11323865c82a1bfca823a52e32c9454194
Wyller Machado <wyller22@gmail.com>
2025-09-18 19:29:52 -0300

    improved `greet_with_flags` formatting

--- commands/greet_with_flags.py
+++ commands/greet_with_flags.py
@@ -5,9 +5,29 @@ from internals.decorator import cli_command
     name="greet-flags",
     help="Greets a person using flags for name and age.",
     arguments=[
-        {"flags": ["-p", "--person"], "kwargs": {"help": "Person's name", "type": str, "required": True}},
-        {"flags": ["-a", "--age"], "kwargs": {"help": "Age in years", "type": int, "required": True}},
-        {"flags": ["-e", "--excited"], "kwargs": {"help": "Make it all caps", "action": "store_true"}},
+        {
+            "flags": ["-p", "--person"],
+            "kwargs": {
+                "help": "Person's name",
+                "type": str,
+                "required": True
+            }
+        },
+        {
+            "flags": ["-a", "--age"],
+            "kwargs": {
+                "help": "Age in years",
+                "type": int,
+                "required": True
+            }
+        },
+        {
+            "flags": ["-e", "--excited"],
+            "kwargs": {
+                "help": "Make it all caps",
+                "action": "store_true"
+            }
+        },
     ],
 )
 def greet_with_flags(args) -> int:
